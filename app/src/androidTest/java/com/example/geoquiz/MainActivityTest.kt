14a4050ed630b8ae84d108eeb557d855e3a42b57
Aaron Grant <aaron.b.grant@gmail.com>
2022-09-26 16:48:20 -0500

    More tests

--- app/src/androidTest/java/com/example/geoquiz/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/geoquiz/MainActivityTest.kt
@@ -35,8 +35,15 @@ class MainActivityTest {
             .check(matches(withText(R.string.question_australia)))
     }
 
-    @Test fun showsSecondQuestionAfterNextPress() {
+    @Test
+    fun showsSecondQuestionAfterNextPress() {
         onView(withId(R.id.next_button)).perform(click())
+        onView(withId(R.id.question_text_view))
+            .check(matches(withText(R.string.question_oceans)))
+    }
+    @Test fun handlesActivityRecreation() {
+        onView(withId(R.id.next_button)).perform(click())
+        scenario.recreate()
         onView(withId(R.id.question_text_view))
             .check(matches(withText(R.string.question_oceans)))
     }
